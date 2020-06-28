e8aad5cd9f9d50fd525450c09b66b2bda930663f
Sreelakshmi K B <52394072+sreekb22@users.noreply.github.com>
2020-06-28 20:57:14 +0530

    Add files via upload

---
+++ trial/main.js
@@ -0,0 +1,26 @@
+
+var feedContainer = document.getElementById("feed");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+  var myRequest = new XMLHttpRequest();
+  myRequest.open('GET', 'https://sreelakshmi.me/tweet/sample.json');
+  myRequest.onload = function() {
+    if (myRequest.status >= 200 && myRequest.status < 400) {
+      alert("sucess");
+      var myData = JSON.parse(myRequest.responseText);
+    //  renderHTML(myData);
+
+    } else {
+      alert("We connected to the server, but it returned an error.");
+    }
+
+  };
+
+  myRequest.onerror = function() {
+    alert("Connection error");
+  };
+
+  myRequest.send();
+
+});
